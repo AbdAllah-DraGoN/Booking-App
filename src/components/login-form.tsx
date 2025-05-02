bc6b0fe86c7cafdde1ec49de640467454ca22add
AbdAllah-DraGoN <abdallahdragon72@gmail.com>
2025-05-02 23:45:38 +0300

    Update Login and singup forms

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -4,11 +4,20 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitClick = () => {
+    // Perform login logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -16,7 +25,7 @@ export function LoginForm({
           <CardTitle className="text-center">Login to your account</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <div>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
@@ -25,27 +34,23 @@ export function LoginForm({
                   type="email"
                   placeholder="m@example.com"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
+                <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   type="password"
                   placeholder="*********"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
+                <Button className="w-full" onClick={handleSubmitClick}>
                   Login
                 </Button>
               </div>
@@ -56,7 +61,7 @@ export function LoginForm({
                 Sign up
               </Link>
             </div>
-          </form>
+          </div>
         </CardContent>
       </Card>
     </div>
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -4,11 +4,23 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function SingUpForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmitClick = () => {
+    console.log("name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("ConfirmPassword:", confirmPassword);
+  };
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -18,11 +30,18 @@ export function SingUpForm({
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <div>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="name">Name</Label>
-                <Input id="name" type="text" placeholder="Your name" required />
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Your name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
@@ -31,6 +50,8 @@ export function SingUpForm({
                   type="email"
                   placeholder="m@example.com"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="grid gap-3">
@@ -40,6 +61,8 @@ export function SingUpForm({
                   type="password"
                   placeholder="*********"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <div className="grid gap-3">
@@ -49,10 +72,12 @@ export function SingUpForm({
                   type="password"
                   placeholder="*********"
                   required
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                 />
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
+                <Button className="w-full" onClick={handleSubmitClick}>
                   Sign up
                 </Button>
               </div>
@@ -63,7 +88,7 @@ export function SingUpForm({
                 Log in
               </Link>
             </div>
-          </form>
+          </div>
         </CardContent>
       </Card>
     </div>
